840d0ba577dc9e4c4ac60b3209f14e5036a83d79
vixataaa <vmetodiev93@gmail.com>
2018-06-15 01:12:01 +0300

    Add gitignore and adjust sample db models

---
+++ MeteoApp.Data.Models/Station.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MeteoApp.Data.Models.Base;
+
+namespace MeteoApp.Data.Models
+{
+    public class Station : DbModel
+    {
+        public string Name { get; set; }
+
+        public virtual ICollection<DayWeatherData> StationData { get; set; }
+        public virtual ICollection<StationWeight> StationWeights { get; set; }
+        public virtual ICollection<StationAvailabilityPeriod> StationAvailabilityPeriods { get; set; }
+
+    }
+}
---
+++ MeteoApp.Data.Models/StationAvailabilityPeriod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MeteoApp.Data.Models.Base;
+
+namespace MeteoApp.Data.Models
+{
+    public class StationAvailabilityPeriod : DbModel
+    {
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+        public virtual int StationId { get; set; }
+        public virtual Station Station { get; set; }
+    }
+}
